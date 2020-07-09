c26b343082a0dd4db0d278313edb1f6c9ee63eb5
Adam Siekierski <adam.siekiera@outlook.com>
2020-07-09 14:42:00 +0200

    Create user object in db on login

--- src/providers/UserProvider.js
+++ src/providers/UserProvider.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 
 export const UserContext = React.createContext({ user: null });
 
@@ -7,7 +7,24 @@ export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    auth.onAuthStateChanged((user) => setUser(user));
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+
+      if (user) {
+        const usersRef = db.collection("users");
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((doc) => {
+            if (!doc.exists) {
+              usersRef.doc(user.uid).set({
+                mod: false,
+                credits: 10,
+              });
+            }
+          });
+      }
+    });
   }, []);
 
   return <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>;
