49c93a16db572233b9dfd1004c65966d434d3e37
Adam Siekierski <adam.siekiera@outlook.com>
2020-07-13 22:33:00 +0200

    Example unit test, and test workflow on PR

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -4,7 +4,7 @@ import Helmet from "react-helmet";
 export default function SEO({ title }) {
   return (
     <Helmet titleTemplate="%s | writes." defaultTitle="writes.">
-      <title>${title}</title>
+      <title>{title || ""}</title>
     </Helmet>
   );
 }
