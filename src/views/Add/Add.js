56e3b05e39f90c60d82f081f3604f97fef1a828a
AleksanderSkubala <alexskubala@gmail.com>
2020-07-16 15:26:18 +0200

    lint fixes

--- src/views/Add/Add.js
+++ src/views/Add/Add.js
@@ -5,7 +5,7 @@ import { db } from "../../firebase";
 import { UserContext } from "../../providers/UserProvider";
 import { newWriteWebhook } from "../../helpers/discord";
 
-import { Link, navigate } from '@reach/router';
+import { Link, navigate } from "@reach/router";
 import SEO from "../../components/SEO/SEO";
 import Layout from "../../components/Layout/Layout";
 
@@ -42,7 +42,7 @@ export default function Add({ visible, setVisible }) {
       .then(() => {
         setLoading(false);
         newWriteWebhook(user, values);
-        navigate('/app');
+        navigate("/app");
       });
   };
 
@@ -57,48 +57,52 @@ export default function Add({ visible, setVisible }) {
 
   return (
     <>
-    <SEO title="Add write" />
+      <SEO title="Add write" />
 
-    <Layout>
-      <Breadcrumb style={{ margin: "16px 0" }}>
-        <Breadcrumb.Item>
-          <Link to="/">Home</Link>
-        </Breadcrumb.Item>
-        <Breadcrumb.Item>Add write</Breadcrumb.Item>
-      </Breadcrumb>
-      <PageContent>
-      <Form
-        name="new-write"
-        labelCol={{ span: 6 }}
-        wrapperCol={{ span: 14 }}
-        onFinish={submitForm}
-        validateMessages={validateForm}
-      >
-        <Form.Item name={["title"]} label="Title" rules={[{ required: true }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item name={["description"]} label="Description" rules={[{ required: true }]}>
-          <Input.TextArea />
-        </Form.Item>
-        <Form.Item name={["content"]} label="Content url" rules={[{ type: "url", required: true }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item name={["tags"]} label="Tags">
-          <Input />
-        </Form.Item>
-        <Form.Item wrapperCol={{ offset: 6 }}>
-          <Space>
-            <Button key="back" onClick={() => navigate('/')}>
-              Cancel
-            </Button>
-            <Button key="submit" htmlType="submit" type="primary" loading={loading}>
-              Submit
-            </Button>
-          </Space>
-        </Form.Item>
-      </Form>
-      </PageContent>
-    </Layout>
+      <Layout>
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>
+            <Link to="/">Home</Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>Add write</Breadcrumb.Item>
+        </Breadcrumb>
+        <PageContent>
+          <Form
+            name="new-write"
+            labelCol={{ span: 6 }}
+            wrapperCol={{ span: 14 }}
+            onFinish={submitForm}
+            validateMessages={validateForm}
+          >
+            <Form.Item name={["title"]} label="Title" rules={[{ required: true }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item name={["description"]} label="Description" rules={[{ required: true }]}>
+              <Input.TextArea />
+            </Form.Item>
+            <Form.Item
+              name={["content"]}
+              label="Content url"
+              rules={[{ type: "url", required: true }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item name={["tags"]} label="Tags">
+              <Input />
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 6 }}>
+              <Space>
+                <Button key="back" onClick={() => navigate("/")}>
+                  Cancel
+                </Button>
+                <Button key="submit" htmlType="submit" type="primary" loading={loading}>
+                  Submit
+                </Button>
+              </Space>
+            </Form.Item>
+          </Form>
+        </PageContent>
+      </Layout>
     </>
   );
 }
